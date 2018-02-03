0f907bbd3029ebff8ad770facfb7abe5bc313bc1
Jose <Jose@Glados-A>
2018-02-03 17:19:44 +0100

    Initial cheking.

---
+++ src/Proyecto_GIT/Calculadora.java
@@ -0,0 +1,88 @@
+/*Modeliza una clase calculadora tendra un atributo resultado.
+ * reset --> poner a 0 resultado
+ * suma --> resutaldo + valor
+ * resta --> resultado - valor
+ * multiplicacion --> resultado * valor
+ * division --> resultado / valor
+ */
+
+package Proyecto_GIT;
+
+import java.io.IOException;
+
+public class Calculadora {
+
+	//Declaracion de atributos
+	private double resultado=0;
+	
+	//Declaracion de constructores:
+	
+	public Calculadora (){
+		
+	}
+	
+	//Declaracion de Metodos
+	
+	public void suma (double a){
+		if(a < Integer.MAX_VALUE && this.resultado < Integer.MAX_VALUE){
+		this.resultado+=a;
+		}
+		else {
+			try {
+				throw new Exception("Habrá overflow si se hace esta suma.");
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+	public void resta (double b) {
+		if(b < Integer.MAX_VALUE && this.resultado < Integer.MAX_VALUE){
+		this.resultado-=b;
+		}
+		else {
+			try {
+				throw new Exception("Habrá overflow con esta resta.");
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+	public void multi(double c){
+		if(c < Integer.MAX_VALUE && this.resultado < Integer.MAX_VALUE){
+		this.resultado*=c;
+		}
+		else{
+			try {
+				throw new Exception("Habrá overflow con esta multiplicación.");
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+	public void div(double d){
+		if(d!=0){
+		this.resultado = this.resultado / d;
+		System.out.println(this.resultado);
+		}
+		else{try {
+			throw new Exception("No se puede dividir por 0");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		}
+}
+	
+	
+	public void reset (String e){
+		
+		this.resultado = 0;
+		
+	}
+	
+	public double retornaResu (){
+	
+		return this.resultado;
+	}
+	
+
+}
---
+++ src/Proyecto_GIT/test.java
@@ -0,0 +1,95 @@
+package Proyecto_GIT;
+import java.util.*;
+import java.io.IOException;
+/**
+ * 
+ * @author Jose y Angelo
+ * @version 1.01
+ * @
+ *
+ */
+
+
+public class test {
+
+	public static void main(String[] args) {
+		int op;
+		double num;
+		String res;
+		Scanner sc= new Scanner(System.in);
+		//Declaracion del objeto
+		Calculadora c1=new Calculadora();
+		
+		do {
+			System.out.println("Por favor elija una de las siguientes opciones.");
+			System.out.println("1. Suma");
+			System.out.println("2. Resta");
+			System.out.println("3. Multiplicación");
+			System.out.println("4. Division");
+			System.out.println("5. Resetear");
+			System.out.println("6. Salir");
+			op=sc.nextInt();
+			switch (op){
+			case 1:
+				System.out.println("Porfavor Introduzca el numero que desea sumar");
+				c1.suma(input(sc.next()));
+				System.out.println("El resultado es: "+c1.retornaResu());
+				break;
+			case 2:
+				System.out.println("Porfavor Introduzca el numero que desea restar");
+				c1.resta(input(sc.next()));
+				System.out.println("El resultado es: "+c1.retornaResu());
+				break;
+			case 3:
+				System.out.println("Porfavor Introduzca el numero que desea multiplicar");
+				c1.multi(input(sc.next()));
+				System.out.println("El resultado es: "+c1.retornaResu());
+				break;
+			case 4:
+				System.out.println("Porfavor Introduzca el numero que desea dividir");
+					c1.div(input(sc.next()));
+					System.out.println("El resultado es: "+c1.retornaResu());
+				
+				break;
+			case 5:
+				c1.reset("Reset");
+				System.out.println("El resultado es: "+c1.retornaResu());
+				break;
+			case 6:
+				op=0;
+				break;
+				
+			default:
+					System.out.println("El numero introducido no es correcto");
+			}
+			
+		}while(op!=0);
+		
+		System.out.println("El resultado es: "+c1.retornaResu());
+		System.out.println("**********Eso es todo Amigos**********");
+	}
+
+	public static double input(String a){
+		if(a.length() <= 13){
+			try{
+				Double.parseDouble(a);
+				return Double.parseDouble(a);
+			}
+			catch(Exception e){
+				System.out.println(e.getMessage()+" Entrada no válida. No son números.");
+				a="0";
+			}
+			}
+		else{
+			try{
+				throw new ArrayIndexOutOfBoundsException(" Error. Longitud inadecuada.  "); 
+			} catch(ArrayIndexOutOfBoundsException e){
+				System.out.println(e);
+			}
+		}
+		return 0;
+	}
+	
+	
+	
+}
